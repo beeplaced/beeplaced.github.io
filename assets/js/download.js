8c6cd3effaff38ba3b7d86f30f90f4cb4aca2e0a
beeplaced <you@example.com>
2024-05-05 13:05:14 +0200

    downloadable

---
+++ assets/js/download.js
@@ -0,0 +1,13 @@
+function downloadPDF() {
+    const contentElement = document.querySelector('.resume');
+    const pdf = new jsPDF();
+
+    // Generate PDF from content
+    pdf.html(contentElement, {
+        callback: function (pdf) {
+            pdf.save('download.pdf'); // Save PDF as 'download.pdf'
+        },
+        x: 10,
+        y: 10
+    });
+}
